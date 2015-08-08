17a58bd7ed06a2e120feb03f92e7c47d9d241bd2
TJ Tanjai <tanjai_k@hotmail.com>
2015-08-08 13:47:30 +0700

    Add dummy to file

--- test_captcha.py
+++ test_captcha.py
@@ -6,19 +6,34 @@ from app import app
 #       self.assertEqual(True,False)
 
 class TestFirstPatternLeftOperand(unittest.TestCase):
+    dummy_operand = 1
+    dummy_right = 1
+    dummy_pattern = 1
+
     def test_1_should_be_1(self):
-        captcha = Captcha(1,1,1,1)
+        captcha = Captcha(self.dummy_pattern,1,self.dummy_operand,self.dummy_right)
         self.assertEqual(captcha.left_operand(), '1')
     def test_2_should_be_2(self):
-        captcha = Captcha(2,2,2,2)
+        captcha = Captcha(self.dummy_pattern,2,self.dummy_operand,self.dummy_right)
         self.assertEqual(captcha.left_operand(), '2')
 
 class TestSecondPatternLeftOperand(unittest.TestCase):
+    def test_1_should_be_one(self):
+        captcha = Captcha(2,1,1,1)
+        self.assertEqual(captcha.left_operand(), 'one')
+
 
 class Captcha:
     def __init__(self,pattern,left,operand,right):
         self.left = left 
+        self.pattern = pattern
 
     def left_operand(self):
-        return str(self.left)
+    
+        if self.pattern == 1:
+            return str(self.left) 
+        elif self.pattern == 2:
+            return 'one'  
+
+
 
