414028dde30ed167a953703c552f7954ac4f31ed
TJ Tanjai <tanjai_k@hotmail.com>
2015-08-08 11:33:37 +0700

    Edit App.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask 
+from test_fizzbuzz import FizzBuzz
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	return "Hello World"
+
+@app.route('/fizzbuzz/<number>')
+def fizzbuzz(number):
+	fizzbuzz = FizzBuzz()
+	return fizzbuzz.count(number)
+
 
 if __name__ == '__main__':
 	app.run()
