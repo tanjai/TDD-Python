c4e72a7ae68223693058791aa8cba4408fdd0ca6
TJ Tanjai <tanjai_k@hotmail.com>
2015-08-08 10:42:45 +0700

    Add fizz buzz when count 3

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,7 +2,9 @@ import unittest
 class FizzBuzz():
 	def count(self, number):
 		if number == 1:
-		   return '1'
+			return '1'
+		elif number == 3:
+			return 'fizz'
 		return '2'
 
 
@@ -15,3 +17,6 @@ class TestFizzBuzz(unittest.TestCase):
 
 	def test_it_should_return_2_when_number_is_1(self):
 		self.assertEqual('2',self.fizzbuzz.count(2))
+
+	def test_it_should_return_fizz_when_number_is_3(self):
+		self.assertEqual('fizz',self.fizzbuzz.count(3))
